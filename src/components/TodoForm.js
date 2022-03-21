7ccb692f6df704f254133fcc4b700ac4058b2214
nldanne <nldanne97@gmail.com>
2022-03-21 17:59:27 +0100

    refactored code

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,20 +1,39 @@
-import React, { useState} from 'react';
+import React from 'react';
 
-function TodoForm() {
-  const [input, setInput] = useState('');
+const TodoForm = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+  const handleInputText = (event) => {
+    setInputText(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, {text: inputText, completed: false, id: Math.floor(Math.random() * 10000) }]);
+    setInputText('');
+  };
+
+  const handleTaskStatus = (event) => {
+    setStatus(event.target.value);
+  };
 
   return (
     <div>
-      <form className='todo-form'>
+      <form className='todo-form' onSubmit={handleSubmit}>
         <input 
           type='text' 
-          placeholder='Add a todo' 
-          value={input}
-          name='text'
+          placeholder='Add a todo'
+          value={inputText}
           className='todo-input'
+          onChange={handleInputText}
         />
-        <button className='todo-button'> Add todo</button>
+        <button className='todo-button' type='submit'> Add todo</button>
       </form>
+        <div className='select'>
+          <select name='todos' className='filter-todo' onChange={handleTaskStatus}>
+            <option value='all'>All</option>
+            <option value='completed'>Completed</option>
+            <option value='uncompleted'>Uncompleted</option>
+          </select>
+        </div>
     </div>
   )
 }
